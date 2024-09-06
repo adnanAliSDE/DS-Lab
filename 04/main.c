d1158bc3cdfcdf50b0f397bb6ede1525006a715b
Mohammad Adnan <adnann3077@gmail.com>
2024-09-06 12:12:07 +0530

    Created a program to demonstrate arithmetic operators.

---
+++ 04/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    int a=13;
+    int b=5;
+    printf("A= %d , B= %d",a,b);
+    printf("\nA+B= %d",a+b);
+    printf("\nA-B= %d",a-b);
+    printf("\nA x B= %d",a*b);
+    printf("\nA / B= %d",a/b);
+    printf("\nA %% B= %d",a%b);
+    return 0;
+}
